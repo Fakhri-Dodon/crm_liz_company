439e0c4a4295f42d970fe8a109b6bfdfb938b853
alifvman <vman.alif217@gmail.com>
2026-01-06 23:45:05 +0700

    drop slug from element

--- app/Models/ProposalElementTemplate.php
+++ app/Models/ProposalElementTemplate.php
@@ -17,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
  * 
  * @property string $id
  * @property string $name
- * @property string $slug
  * @property array $content_json
  * @property string $preview_image
  * @property string|null $html_output
@@ -51,7 +50,6 @@ class ProposalElementTemplate extends Model
 	protected $fillable = [
 		'name',
 		'slug',
-		'content_json',
 		'preview_image',
 		'html_output',
 		'css_output',
