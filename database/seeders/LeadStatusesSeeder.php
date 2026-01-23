97b252f034583289618be3a1899f332cceb410e6
zackykeisya <adibadina642@gmail.com>
2026-01-23 21:25:32 +0700

    add api filtering upload_at

--- database/seeders/LeadStatusesSeeder.php
+++ database/seeders/LeadStatusesSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\LeadStatuses;
-use Illuminate\Support\Str;
 
 class LeadStatusesSeeder extends Seeder
 {
@@ -16,26 +15,28 @@ class LeadStatusesSeeder extends Seeder
     {
         $allStatus = [
             [
+                'id' => '30f0597e-bf13-4834-b4b9-7d32f267748e', // ID FIXED untuk New
                 'name'  => 'New',
                 'color' => '#3B82F6',
-                'color_name'    => 'blue'
+                'color_name' => 'blue',
+                'order' => 1,
+                'is_system' => 1
             ],
             [
+                'id' => '71917151-2f42-428c-9774-4818d5b46f6b', // ID FIXED untuk Sent
                 'name'  => 'Sent',
                 'color' => '#22C55E',
-                'color_name'    => 'Green'
+                'color_name' => 'Green',
+                'order' => 2,
+                'is_system' => 1
             ],
         ];
 
         foreach ($allStatus as $status) {
-            LeadStatuses::create([
-                'id' => Str::uuid()->toString(),
-                'name' => $status['name'],
-                'color' => $status['color'],
-                'color_name' => $status['color_name'],
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            LeadStatuses::updateOrCreate(
+                ['id' => $status['id']], // Cari berdasarkan ID tetap
+                $status
+            );
         }
     }
 }
